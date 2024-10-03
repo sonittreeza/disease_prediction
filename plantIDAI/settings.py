d030a6aa7535b12cc12a7fbb3002b3ca1b5a9c97
sonittreeza <115927217+sonittreeza@users.noreply.github.com>
2024-10-03 22:22:25 +0530

    Update settings.py

--- plantIDAI/settings.py
+++ plantIDAI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-do!a!3dwz4j1j!tr-s!p543@=x#b__2vg=n2gkxjg9gt+tx%m8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
